1c97b05305616e3184f69dffd591d59f6a2957be
blues0514 <blues0514@naver.com>
2019-07-20 18:50:31 +0900

    Add Loop code [ADD]

---
+++ src/First/Prac_Digits.java
@@ -0,0 +1,24 @@
+package First;
+
+import java.util.Scanner;
+
+public class Prac_Digits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("양의 정수 자릿수 구하기");
+        int n;
+        do {
+            System.out.print("정수 값 :");
+            n = scanner.nextInt();
+        } while (n < 0);
+
+        int dig = 0;
+
+        while (n > 0) {
+            n /= 10;
+            dig++;
+        }
+        System.out.println("입력한 정수의 자릿수는 " + dig + "자리 입니다.");
+    }
+}
