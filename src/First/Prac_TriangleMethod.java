cfb6d78786af2ed05c38c75091ecc23353634824
blues0514 <blues0514@naver.com>
2019-07-20 19:43:29 +0900

    Add Figure code [ADD] + [Finish First Package]

---
+++ src/First/Prac_TriangleMethod.java
@@ -0,0 +1,29 @@
+package First;
+
+import java.util.Scanner;
+
+public class Prac_TriangleMethod {
+
+    // 왼쪽 아래가 직각인 이등변 삼각형 출력
+    static void triangleLB(int n) {
+        for (int i =1; i<=n ; i++){
+            for(int j =1; j<=i ; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+
+        System.out.println("삼각형 만들기");
+        do{
+            System.out.println("양의 정수를 입력하세요");
+            num = scanner.nextInt();
+        } while (num <=0);
+
+        triangleLB(num);
+    }
+}
