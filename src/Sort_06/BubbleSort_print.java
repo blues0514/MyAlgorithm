9039ab05bec3fe53790984ec5afafff3c7292e64
blues0514 <blues0514@naver.com>
2019-08-11 18:36:40 +0900

    Add BubbleSort_print, ShakerSort code [ADD]

--- src/Sort_06/BubbleSort_print.java
+++ src/Sort_06/BubbleSort_print.java
@@ -67,7 +67,39 @@ public class BubbleSort_print {
         System.out.println("교환을 " + scnt + "회 했습니다.");
     }
 
-    //버전 3 추가 예정
+    // 버전3
+    static void bubbleSort3(int[] a, int n) {
+        int ccnt = 0; // 비교 횟수
+        int scnt = 0; // 교환 횟수
+
+        int i = 0;
+        int k = 0;
+
+        while (k < n - 1) {
+            System.out.printf("패스%d：\n", i++);
+            int last = n - 1;
+            for (int j = n - 1; j > k; j--) {
+                for (int m = 0; m < n - 1; m++)
+                    System.out.printf("%3d %c", a[m], (m != j - 1) ? ' ' : (a[j - 1] > a[j] ? '+' : '-'));
+                System.out.printf("%3d\n", a[n - 1]);
+
+                ccnt++;
+
+                if (a[j - 1] > a[j]) {
+                    swap(a, j - 1, j);
+                    last = j;
+                    scnt++;
+                }
+            }
+            k = last;
+        }
+        for (int m = 0; m < n; m++)
+            System.out.printf("%3d  ", a[m]);
+        System.out.println();
+
+        System.out.println("비교를 " + ccnt + "회 했습니다.");
+        System.out.println("교환을 " + scnt + "회 했습니다.");
+    }
 
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
@@ -82,6 +114,6 @@ public class BubbleSort_print {
             x[i] = stdIn.nextInt();
         }
 
-        bubbleSort(x, nx); // 배열 x를 단순교환정렬
+        bubbleSort3(x, nx); // 배열 x를 단순교환정렬
     }
 }
