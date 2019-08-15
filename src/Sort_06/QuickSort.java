3e9bd5d2f08ea908f92af25116bb7bb577b00bfd
blues0514 <blues0514@naver.com>
2019-08-15 17:07:56 +0900

    Add QuickSort3 code [ADD]

--- src/Sort_06/QuickSort.java
+++ src/Sort_06/QuickSort.java
@@ -1,5 +1,7 @@
 package Sort_06;
 
+import Queue_Stack_04.IntStack;
+
 import java.util.Scanner;
 
 /*
@@ -21,7 +23,7 @@ public class QuickSort {
         int x = a[(pl + pr) / 2]; // 피벗
 
         System.out.printf("a[%d]~a[%d] : {", left, right);
-        for ( int i = left ; i < right; i++)
+        for (int i = left; i < right; i++)
             System.out.printf("%d , ", a[i]);
         System.out.printf("%d}\n", a[right]);
 
@@ -40,6 +42,52 @@ public class QuickSort {
         quickSort(a, 0, n - 1);
     }
 
+    // 비재귀적인 퀵 정렬
+    static void quickSort3(int[] a, int left, int right) {
+        IntStack lstack = new IntStack(right - left + 1);
+        IntStack rstack = new IntStack(right - left + 1);
+
+        lstack.push(left);
+        rstack.push(right);
+        System.out.printf("a[%d] ~ a[%d]를 분할하는 스택에 푸시\n", left, right);
+        System.out.print("LStack : ");
+        lstack.dump();
+        System.out.print("RStack : ");
+        rstack.dump();
+
+        while (lstack.isEmpty() != true) {
+            int pl = left = lstack.pop(); // 왼쪽 커서
+            int pr = right = rstack.pop(); // 오른쪽 커서
+            int x = a[(left + right) / 2]; // 피벗
+            System.out.printf("a[%d] ~ a[%d]을 분할하는 스택을 팝 \n", left, right);
+            do {
+                while (a[pl] < x) pl++;
+                while (a[pr] > x) pr--;
+                if (pl <= pr)
+                    swap(a, pl++, pr--);
+            } while (pl <= pr);
+
+            if (left < pr) {
+                lstack.push(left); // 왼쪽 그룹 범위의
+                rstack.push(pr); // 인덱스를 푸시
+                System.out.printf("a[%d] ~ a[%d]를 분할하는 스택에 푸시\n", left, pr);
+                System.out.print("LStack : ");
+                lstack.dump();
+                System.out.print("RStack : ");
+                rstack.dump();
+            }
+            if ( pl < right) {
+                lstack.push(pl); // 오른쪽 그룹 범위의
+                rstack.push(right); // 인덱스를 푸시
+                System.out.printf("a[%d] ~ a[%d]를 분할하는 스택에 푸시\n", pl, right);
+                System.out.print("LStack : ");
+                lstack.dump();
+                System.out.print("RStack : ");
+                rstack.dump();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -53,7 +101,8 @@ public class QuickSort {
             x[i] = scanner.nextInt();
         }
         //quickSort(x, 0, nx - 1);
-        quickSort2(x, nx);
+        //quickSort2(x, nx);
+        quickSort3(x,0,nx-1);
 
         System.out.println("오름차순으로 정렬했습니다.");
         for (int i = 0; i < nx; i++)
